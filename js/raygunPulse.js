4630812a56682a04b3f90d571943e8214be703ff
PRIJCK Frederik (FPRJ) <frederik.prijck@gmail.com>
2016-05-30 16:55:37 +0200

    removed console.log

--- js/raygunPulse.js
+++ js/raygunPulse.js
@@ -2,7 +2,6 @@
 
     return {
         init: function (options) {
-            global.console.log("calling rg4js: " + options.apiKey +" with: " + options.identifier);
             global.rg4js('apiKey', options.apiKey);
             global.rg4js('enablePulse', true);
             global.rg4js('setUser', {
